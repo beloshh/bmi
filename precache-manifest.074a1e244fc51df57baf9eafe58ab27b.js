9f4867d17bf86ebd09bb09cc50ac782828e6f246
badanga ishak <badangabello@gmail.com>
2020-12-09 00:42:43 -0800

    Updates

---
+++ precache-manifest.074a1e244fc51df57baf9eafe58ab27b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33a3f29f3a7b91968c46d7145bd716c",
+    "url": "/bmi/index.html"
+  },
+  {
+    "revision": "01fdfe088ac715b4a1c6",
+    "url": "/bmi/static/css/main.e400fdc2.chunk.css"
+  },
+  {
+    "revision": "49b98013d8aa9fc7e7a4",
+    "url": "/bmi/static/js/2.e8fad5ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bmi/static/js/2.e8fad5ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01fdfe088ac715b4a1c6",
+    "url": "/bmi/static/js/main.e352c2d2.chunk.js"
+  },
+  {
+    "revision": "285bf2ed5415dd315669",
+    "url": "/bmi/static/js/runtime-main.a94da0a6.js"
+  },
+  {
+    "revision": "df6601b0961197fd570b44815ac44c68",
+    "url": "/bmi/static/media/bg-header-desktop.df6601b0.png"
+  }
+]);
